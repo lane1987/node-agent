4e1be1dca569abad44f6d2855b79ec98911527c0
matyhtf <mikan.tenny@gmail.com>
2015-08-12 15:02:11 +0800

    增加加密

--- client.php
+++ client.php
@@ -6,7 +6,7 @@
 require_once __DIR__ . '/Swoole/NodeAgent/Base.php';
 require_once __DIR__ . '/Swoole/NodeAgent/Client.php';
 
-$encrypt_key = 'f05dbd87f09a1843c6579aa47e65019b7e089738187c791ab9db2fff0530fa97fb57e5ff8222c3e360521d1f6429fa8f';
+$encrypt_key = file_get_contents(__DIR__.'/encrypt.key');
 $client = new \Swoole\NodeAgent\Client($encrypt_key);
 $args = getopt("p:h:f:t");
 
--- node_server.php
+++ node_server.php
@@ -2,8 +2,8 @@
 require_once __DIR__ . '/Swoole/NodeAgent/Base.php';
 require_once __DIR__ . '/Swoole/NodeAgent/Server.php';
 
-$encrypt_key = 'f05dbd87f09a1843c6579aa47e65019b7e089738187c791ab9db2fff0530fa97fb57e5ff8222c3e360521d1f6429fa8f';
-//$encrypt_key = '';
+//是一个96字节的文件
+$encrypt_key = file_get_contents(__DIR__.'/encrypt.key');
 $svr = new Swoole\NodeAgent\Server($encrypt_key);
 //设置上传文件的存储目录
 $svr->setRootPath('/data/testnode/');
