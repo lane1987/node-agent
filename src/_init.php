f8b415d3b8b050efa5593c2b31774e8f7d3610d9
matyhtf <mikan.tenny@gmail.com>
2015-08-27 19:41:13 +0800

    配置独立

--- src/_init.php
+++ src/_init.php
@@ -1,4 +1,14 @@
 <?php
 define('WEBPATH', __DIR__);
 require_once __DIR__ . '/framework/libs/lib_config.php';
+
+$env = get_cfg_var('env.name');
+$env = "dev";
+if (empty($env))
+{
+    $env = 'product';
+}
+define('ENV_NAME', $env);
+
+Swoole::$php->config->setPath(__DIR__ . '/apps/configs/' . ENV_NAME);
 Swoole\Loader::addNameSpace('NodeAgent', __DIR__ . '/NodeAgent');
