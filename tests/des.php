7f873bbe88475c7f370e61bda16aee022e63260c
lane <61647649@qq.com>
2016-03-03 08:04:35 +0800

    modify des.php

--- tests/des.php
+++ tests/des.php
@@ -7,3 +7,4 @@ $des = new \NodeAgent\Base($encrypt_key);
 $data = $des->pack(['code' => '400', 'file' => __FILE__, 'hello' => 'world']);
 var_dump($data);
 var_dump($des->unpack($data));
+echo "OK\n";
